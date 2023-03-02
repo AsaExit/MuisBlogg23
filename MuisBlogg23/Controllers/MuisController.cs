8ee74f0f2c45803168834526302afa83be988355
Åsa Jonsson <asaj71@gmail.com>
2023-03-02 14:21:03 +0100

    Add project files.

---
+++ MuisBlogg23/Controllers/MuisController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MuisBlogg23.Models;
+
+namespace MuisBlogg23.Controllers
+{
+   
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MuisController : ControllerBase
+    {
+        private readonly IMongoCollection<Muis> _muis;
+        public MuisController(IOptions<DatabasSettings> settings)
+        {
+            var mongoClient = new MongoClient(
+            settings.Value.ConnectionString); var mongoDatabase = mongoClient.GetDatabase(
+            settings.Value.DatabaseName); _muis = mongoDatabase.GetCollection<Muis>(
+            settings.Value.MuisCollectionName);
+        }
+        [HttpGet]
+        public async Task Getall()
+        {
+            await _muis.Find(_=>true).ToListAsync();
+
+        }
+        [HttpPost]
+        public async Task createMuisPost(Muis muis)
+        {
+            await _muis.InsertOneAsync(muis);
+          
+        } 
+    }
+
+ 
+    
+}
+ 
+
---
+++ MuisBlogg23/Models/Muis.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MuisBlogg23.Models
+{
+    public class Muis
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+         public string Namn{ get; set; }   
+    }
+}
