e3407ebbf55958ddd1055d2aa33075d9fb8a661a
AlexanderonGit <alexanderhollgren@gmail.com>
2023-03-02 15:05:31 +0100

    MUIS SUPRISE

--- MuisBlogg23/Models/Muis.cs
+++ MuisBlogg23/Models/Muis.cs
@@ -7,8 +7,25 @@ namespace MuisBlogg23.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-        public string Namn { get; set; }   
-
+        private string Id { get; set; }
+        public string Namn { get; set; }
+        public string CowType { get; private set; } = Random.Shared.Next(12) switch
+        {
+              0 => "Angus",
+            < 1 => "Hereford",
+            < 2 => "Highland Cattle",
+            < 3 => "Rödkulla",
+            < 4 => "Blonde d’Aquitaine",
+            < 5 => "Charolais",
+            < 6 => "Limousin",
+            < 7 => "Simmental",
+            < 8 => "Svensk Röd och vit boskap",
+            < 9 => "Svensk Låglandsboskap",
+            < 10 => "Svensk kullig boskap",
+            < 11 => "Svensk Jersey-boskap",
+            < 12 => "SUPER MUIS",
+            _=>"SOMETHING WRONG ERROR ERROR"
+        };
     }
+
 }
